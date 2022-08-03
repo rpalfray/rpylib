62e63251cd671ffb2f6658cfe667418e88dea4fc
rpalfray <romain.palfray@gmail.com>
2022-08-03 16:48:52 +0200

    remove init file

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""rpylib is a pricing library originally developed to price jump processes with Monte-Carlo and
-Multi-level Monte-Carlo methods via a Continuous Markov Chain approximation"""
-
-
-__version__ = '1.0.0'
