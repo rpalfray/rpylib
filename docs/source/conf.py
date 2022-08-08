6826f9df6ae4b97d5f2dc81c42e420490945e878
rpalfray <romain.palfray@gmail.com>
2022-08-08 16:36:45 +0200

    applying black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,31 +8,28 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../rpylib'))
 
-project = 'rpylib'
-copyright = '2022, Romain Palfray'
-author = 'Romain Palfray'
-version = '1.0'
-release = '1.0'
+sys.path.insert(0, os.path.abspath("../../rpylib"))
+
+project = "rpylib"
+copyright = "2022, Romain Palfray"
+author = "Romain Palfray"
+version = "1.0"
+release = "1.0"
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.todo'
-]
+extensions = ["sphinx.ext.duration", "sphinx.ext.autodoc", "sphinx.ext.todo"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'nature'
-html_static_path = ['_static']
+html_theme = "nature"
+html_static_path = ["_static"]
 
 add_module_names = False
--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 import io
 import os
 import sys
@@ -12,8 +11,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 
 def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
+    encoding = kwargs.get("encoding", "utf-8")
+    sep = kwargs.get("sep", "\n")
     buf = []
     for filename in filenames:
         with io.open(filename, encoding=encoding) as f:
@@ -21,7 +20,7 @@ def read(*filenames, **kwargs):
     return sep.join(buf)
 
 
-long_description = read('README.rst')
+long_description = read("README.rst")
 
 
 class PyTest(TestCommand):
@@ -32,34 +31,35 @@ class PyTest(TestCommand):
 
     def run_tests(self):
         import pytest
+
         errcode = pytest.main(self.test_args)
         sys.exit(errcode)
 
 
 setup(
-    name='rpylib',
+    name="rpylib",
     version=rpylib.__version__,
-    url='https://github.com/rpalfray/rpylib',
-    license='GNU General Public licence',
-    author='Romain Palfray',
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
-    author_email='romain.palfray@gmail.com',
-    description='Pricing library',
+    url="https://github.com/rpalfray/rpylib",
+    license="GNU General Public licence",
+    author="Romain Palfray",
+    tests_require=["pytest"],
+    cmdclass={"test": PyTest},
+    author_email="romain.palfray@gmail.com",
+    description="Pricing library",
     long_description=long_description,
-    packages=['rpylib'],
+    packages=["rpylib"],
     include_package_data=True,
-    platforms='any',
-    test_suite='rpylib.tests',
+    platforms="any",
+    test_suite="rpylib.tests",
     classifiers=[
-        'Programming Language :: Python',
-        'Development Status :: 2 - Pre-Alpha',
-        'Natural Language :: English',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
+        "Programming Language :: Python",
+        "Development Status :: 2 - Pre-Alpha",
+        "Natural Language :: English",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
     extras_require={
-        'testing': ['pytest'],
-    }
+        "testing": ["pytest"],
+    },
 )
